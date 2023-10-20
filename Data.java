2de24e5e80f31af33873598834687f319bcebe06
NamrataCSalvi <147251955+NamrataCSalvi@users.noreply.github.com>
2023-10-20 18:12:36 +0530

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,66 +1,94 @@
 
+package com.example;
 
-import java.sql.Blob;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
-public class Data {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("user-form")
+public class Data extends HttpServlet {
 
     public static void main(String[] args) {
-        // Replace "jdbc:mysql://localhost:3306/database_name" with the appropriate database connection string
-        String connectionUrl = "jdbc:mysql://localhost:3306/mproject";
-       
-        // Replace "root" and "password" with the appropriate database username and password
-        String username = "root";
-        String password = "12345";
-
-        // Assuming 'request' represents a form request object (you should adapt this to your framework)
-        request request = new request();
-
-        String name = request.getParameter("txtName");
-        String email = request.getParameter("txtEmail");
-        Blob image = request.getimBlob("imageinput");
-        int pass = request.getIntegerPassword("passw");
 
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Replace "jdbc:mysql://localhost:3306/database_name" with the appropriate
+            // database connection string
+            String connectionUrl = "jdbc:mysql://localhost:3306/mproject";
+
+            // Replace "root" and "password" with the appropriate database username and
+            // password
+            String username = "root";
+            String password = "12345";
+
+            // Assuming 'request' represents a form request object (you should adapt this to
+            // your framework)
+            request request = new request();
+
+            String name = request.getParameter("txtName");
+            String email = request.getParameter("txtEmail");
+            Blob image = request.getimBlob("imageinput");
+            int pass = request.getIntegerPassword("passw");
+
             // Create a connection to the database
             Connection connection = DriverManager.getConnection(connectionUrl, username, password);
-            
 
-            // Create a prepared statement to insert the form data into the database
-            String sql = "INSERT INTO itable3 (name, email, image, pass) VALUES (username, email, uimage, passw)";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, email);
-            preparedStatement.setBlob(3, image);
-            preparedStatement.setInt(4, pass);
+            String q = "insert into itable3 (username,email,uimage,passw) values (?,?,?,?)";
 
-            // Execute the prepared statement
-            preparedStatement.executeUpdate();
-
-            // Close the prepared statement and the connection
-            preparedStatement.close();
+            PreparedStatement pstm = connection.prepareStatement(q);
+            pstm.setString(1, name);
+            pstm.setString(2, email);
+            pstm.setBlob(3, image);
+            pstm.setInt(4, pass);
+            pstm.executeUpdate();
+            System.out.println("Data inserted successfully!");
             connection.close();
 
-            System.out.println("Data stored successfully");
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+
         }
+        // Create a prepared statement to insert the form data into the database
+        // String sql = "INSERT INTO itable3 (username, email, uimage, passw) VALUES
+        // (name, email, image, pass)";
+        // PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        // preparedStatement.setString(1, name);
+        // preparedStatement.setString(2, email);
+        // preparedStatement.setBlob(3, image);
+        // preparedStatement.setInt(4, pass);
+
+        // Execute the prepared statement
+        // preparedStatement.executeUpdate();
+
+        // Close the prepared statement and the connection
+        // preparedStatement.close();
+        // c//onnection.close();
+        // if (connection.isClosed()) {
+        // System.out.println("not connected");
+        // }
+
+        // else {
+        // System.out.println("Data stored successfully");
+        // }
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
     }
 }
 
 class request {
-    // Implement your 'getParameter' method to return the form data as needed
+
     public String getParameter(String paramName) {
-        // This is a placeholder; implement your logic here to retrieve form data
+
         return null;
     }
+
     public Blob getimBlob(String imageParamName) {
-           return null;
+        return null;
     }
-    public int getIntegerPassword (int passwordParamName) {
+
+    public int getIntegerPassword(String passwordParamName) {
         return 0;
-        }
+    }
 }
